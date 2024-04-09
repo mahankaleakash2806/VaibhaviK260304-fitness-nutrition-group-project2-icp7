7ca5213ad9a9204f31f4b095dadbbf04433167a2
RohiniG-12 <garudkarrohini0170@gmail.com>
2024-04-09 16:30:44 +0530

    css added

--- js/login.js
+++
@@ -1,5 +0,0 @@
-const  sign_in_btn=document.querySelector("#sign_in_btn");
-const  sign_up_btn=document.querySelector("#sign_up_btn");
-const  container=document.querySelector(".container");
-
-sign_up_btn.addEventListener("click",()=>)
