b8e13e463fff1da0957ca65663bcf2debc033c85
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-04-10 00:50:46 +0530

    Set timeout for routine container

--- js/gym_facilities.js
+++ js/gym_facilities.js
@@ -1,4 +1,7 @@
 function showRoutine(){
     const  routine = document.getElementById("daily-routine");
     routine.classList.add("d-block");
+    setTimeout(() => {
+        routine.classList.remove("d-block");
+    }, 10000);
 }
