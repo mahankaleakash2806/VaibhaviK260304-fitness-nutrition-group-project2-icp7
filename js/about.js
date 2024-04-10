65013cb9606d447bc617ed1195fd9b128aeed988
Vaishugarudkar01 <vaishugarudkar2005@gmail.com>
2024-04-10 12:49:36 +0530

    changes added

---
+++ js/about.js
@@ -0,0 +1,21 @@
+
+function slider1(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../img/about-page/gym.png' ;
+}
+function slider2(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../img/about-page/fitness.png';
+}
+function slider3(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src = '../img/about-page/nutrition.png';
+}
+function slider4(){
+    const slideChanger = document.getElementById("slider");
+
+    slideChanger.src ='../img/about-page/health.png';
+}
