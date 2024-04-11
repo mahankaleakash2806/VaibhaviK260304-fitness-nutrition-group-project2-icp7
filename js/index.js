49877bc97333175779e8764545d37a8d338aa02e
123vkgithub <kumbhalkarvaibhavi@gmail.com>
2024-04-11 13:01:59 +0530

    created new java file style.js

---
+++ js/index.js
@@ -0,0 +1,49 @@
+
+function toggleBookmark() 
+{
+    let bookmark = document.getElementById("bookmark");
+    if (bookmark.src.match("./icons/home/bookmark-white.png")) 
+    {
+      bookmark.src = "./icons/home/bookmark.png";
+    } 
+    else 
+    {
+      bookmark.src = "./icons/home/bookmark-white.png";
+    }
+}
+function toggleBookmark1() 
+{
+    let bookmark1 = document.getElementById("bookmark1");
+    if (bookmark1.src.match("./icons/home/bookmark-white.png")) 
+    {
+      bookmark1.src = "./icons/home/bookmark.png";
+    } 
+    else 
+    {
+      bookmark1.src = "./icons/home/bookmark-white.png";
+    }
+}
+function toggleBookmark2() 
+{
+    let bookmark2 = document.getElementById("bookmark2");
+    if (bookmark2.src.match("./icons/home/bookmark-white.png")) 
+    {
+      bookmark2.src = "./icons/home/bookmark.png";
+    } 
+    else 
+    {
+      bookmark2.src = "./icons/home/bookmark-white.png";
+    }
+}
+function toggleBookmark3() 
+{
+    let bookmark3 = document.getElementById("bookmark3");
+    if (bookmark3.src.match("./icons/home/bookmark-white.png")) 
+    {
+      bookmark3.src = "./icons/home/bookmark.png";
+    } 
+    else 
+    {
+      bookmark3.src = "./icons/home/bookmark-white.png";
+    }
+}
